8d0907dd32a6815b849a8eb936315f4c5d8ae49c
Remy <remy.sohier@gmail.com>
2019-04-22 17:54:48 +0200

    some stuffs on jockey creation
    Signed-off-by: Remy <remy.sohier@gmail.com>

---
+++ Assets/Gamejockey/Editor/GameJockeyEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace GameJockey_v4
+{
+    [CustomEditor(typeof(GameJockey))]
+    public class GameJockeyEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            GameJockey jockey = (GameJockey)target;
+
+            GUILayout.Space(5);
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Track A");
+            GUILayout.Label("Track B");
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+
+            // play left track A
+            if (GUILayout.Button("Play"))
+            {
+                jockey.LoadTrack(0);
+            }
+
+            // play left track B
+            if (GUILayout.Button("Play"))
+            {
+                jockey.LoadTrack(1);
+            }
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}
--- Assets/Gamejockey/Source/GameJockey.cs
+++ Assets/Gamejockey/Source/GameJockey.cs
@@ -7,20 +7,68 @@ namespace GameJockey_v4
     public class GameJockey : MonoBehaviour
     {
         public GameSample[] samples;
-        public List<GameSample> tracks;
+        public GameTrack trackA, trackB;
 
         // Péripherique
 
+        // Players
+        public int activePlayers = 4; // issue if we change active player in live + issue if we load track A or B with other player number
+
         // Methods
         // Create a track with selected sample by creating all assets and behaviour during time
-        public void LoadTrack(GameSample _sample)
+        public void LoadTrack(int _index)
         {
+            if(_index == 0)
+            {
+                if(trackA.sceneTarget == null)
+                {
+                    trackA.sceneTarget = new GameObject();
+                    trackA.sceneTarget.name = "TRACK A";
+                    CreateTrackContent(trackA);
+                }
+                else
+                {
+                    DestroyImmediate(trackA.sceneTarget);
+                    trackA.sceneTarget = null;
+                }
+            }
+            else
+            {
+                if (trackB.sceneTarget == null)
+                {
+                    trackB.sceneTarget = new GameObject();
+                    trackB.sceneTarget.name = "TRACK B";
+                    CreateTrackContent(trackB);
+                }
+                else
+                {
+                    DestroyImmediate(trackB.sceneTarget);
+                    trackB.sceneTarget = null;
+                }
+                
+            }
+        }
 
+        void CreateTrackContent(GameTrack _track)
+        {
+            // player creation
+            /*for(int i=0; i<activePlayers; i++)
+            {
+                List<GameObject> _players;
+            }*/
         }
 
         public void UnloadTrack(int _pistIndex)
         {
 
         }
+    }
+
+    [System.Serializable]
+    public class GameTrack
+    {
+        public GameSample sample;
+        public GameObject sceneTarget;
+        public GameObject[] players;
     }
 }
